d2456709c09b8ab07875634f298bfdb82384d0fc
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-07-09 23:42:29 +0700

    Add registration functionality #1

---
+++ src/GymBackend.UseCases/Users/RegistrationUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using GymBackend.Domain.Users.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Saritasa.Tools.Domain.Exceptions;
+
+namespace GymBackend.UseCases.Users.RegistrationUser;
+
+/// <summary>
+/// Handler for <see cref="RegisterUserCommand"/>.
+/// </summary>
+public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, Guid>
+{
+    private readonly UserManager<User> userManager;
+    private readonly ILogger<RegisterUserCommandHandler> logger;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public RegisterUserCommandHandler(
+        UserManager<User> userManager,
+        ILogger<RegisterUserCommandHandler> logger)
+    {
+        this.userManager = userManager;
+        this.logger = logger;
+    }
+
+    /// <inheritdoc />
+    public async Task<Guid> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        var newUser = new User()
+        {
+            Email = request.Email,
+            UserName = request.Email,
+        };
+
+        var result = await userManager.CreateAsync(newUser, request.Password);
+
+        if (!result.Succeeded)
+        {
+            throw new DomainException("Error in login or password");
+        }
+
+        var userId = await userManager.GetUserIdAsync(newUser);
+        return Guid.Parse(userId);
+    }
+}
---
+++ src/GymBackend.Web/Controllers/RegistrationController.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GymBackend.UseCases.Users.RegistrationUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymBackend.Web.Controllers;
+
+/// <summary>
+/// Authentication controller.
+/// </summary>
+[ApiController]
+[Route("api/register")]
+[ApiExplorerSettings(GroupName = "Registration")]
+public class RegistrationController
+{
+    private readonly IMediator mediator;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public RegistrationController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    /// <summary>
+    /// Classic registration.
+    /// </summary>
+    /// <param name="command">Register command.</param>
+    /// <param name="cancellationToken">Cancellation token to cancel the request.</param>
+    [HttpPost]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400)]
+    public async Task Register([Required] RegisterUserCommand command, CancellationToken cancellationToken)
+    {
+        await mediator.Send(command, cancellationToken);
+    }
+}
