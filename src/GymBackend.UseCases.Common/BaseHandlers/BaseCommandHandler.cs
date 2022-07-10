2aeea2f73b9f0aecf64f285683a68309556d02d1
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-07-10 14:39:04 +0700

    Add base handlers #3

---
+++ src/GymBackend.UseCases.Common/BaseHandlers/BaseCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using GymBackend.Infrastructure.Abstractions.Interfaces;
+using Saritasa.Tools.Common.Utils;
+
+namespace GymBackend.UseCases.Common.BaseHandlers;
+
+/// <summary>
+/// Base logic for commands.
+/// </summary>
+public abstract class BaseCommandHandler
+{
+    /// <summary>
+    /// Automapper.
+    /// </summary>
+    protected IMapper Mapper { get; }
+
+    /// <summary>
+    /// Database context.
+    /// </summary>
+    protected IAppDbContext DbContext { get; }
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public BaseCommandHandler(IMapper mapper, IAppDbContext dbContext)
+    {
+        Guard.IsNotNull(mapper, nameof(mapper));
+        Guard.IsNotNull(dbContext, nameof(dbContext));
+
+        Mapper = mapper;
+        DbContext = dbContext;
+    }
+}
