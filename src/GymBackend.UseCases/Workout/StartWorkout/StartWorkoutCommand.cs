aa2d79fc6ee51575a0370416601aeecf2f4c834f
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-08-14 23:48:59 +0700

    Add command for start workout #9

---
+++ src/GymBackend.UseCases/Workout/StartWorkout/StartWorkoutCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace GymBackend.UseCases.Workout.StartWorkout;
+
+/// <summary>
+/// Start workout command.
+/// </summary>
+public record StartWorkoutCommand() : IRequest<Guid>;
---
+++ src/GymBackend.UseCases/Workout/StartWorkout/StartWorkoutCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using GymBackend.Domain.Workouts;
+using GymBackend.Infrastructure.Abstractions.Interfaces;
+using GymBackend.UseCases.Common.BaseHandlers;
+using MediatR;
+
+namespace GymBackend.UseCases.Workout.StartWorkout;
+
+/// <summary>
+/// Handler for <see cref="StartWorkoutCommand"/>.
+/// </summary>
+internal class StartWorkoutCommandHandler : BaseCommandHandler, IRequestHandler<StartWorkoutCommand, Guid>
+{
+    private readonly ILoggedUserAccessor loggedUserAccessor;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public StartWorkoutCommandHandler(IMapper mapper, IAppDbContext dbContext, ILoggedUserAccessor loggedUserAccessor)
+        : base(mapper, dbContext)
+    {
+        this.loggedUserAccessor = loggedUserAccessor;
+    }
+
+    /// <inheritdoc />
+    public async Task<Guid> Handle(StartWorkoutCommand request, CancellationToken cancellationToken)
+    {
+        var workout = new Domain.Workouts.Workout()
+        {
+            CreatedById = loggedUserAccessor.GetCurrentUserId(),
+            CreatedAt = DateTime.UtcNow,
+            WorkoutStatus = WorkoutStatus.InProgress
+        };
+        DbContext.Workouts.Add(workout);
+        await DbContext.SaveChangesAsync(cancellationToken);
+
+        return workout.Id;
+    }
+}
