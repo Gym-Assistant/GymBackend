435741cabbbae860e0bbc106d2701208c9a0a5d1
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-08-28 01:19:58 +0700

    Add dtos for train session, workout, exercise and sets #9

---
+++ src/GymBackend.UseCases.Common/Dtos/Workout/LightExerciseDto.cs
@@ -0,0 +1,32 @@
+﻿namespace GymBackend.UseCases.Common.Dtos.Workout;
+
+/// <summary>
+/// Light exercise dto.
+/// </summary>
+public record LightExerciseDto
+{
+    /// <summary>
+    /// Id.
+    /// </summary>
+    public Guid Id { get; init; }
+
+    /// <summary>
+    /// Name of exercise.
+    /// </summary>
+    public string Name { get; init; }
+
+    /// <summary>
+    /// Description of exercise.
+    /// </summary>
+    public string Description { get; init; }
+
+    /// <summary>
+    /// User Id.
+    /// </summary>
+    public Guid CreatedById { get; init; }
+
+    /// <summary>
+    /// Created At.
+    /// </summary>
+    public DateTime CreatedAt { get; init; }
+}
---
+++ src/GymBackend.UseCases.Common/Dtos/Workout/SetsDto.cs
@@ -0,0 +1,32 @@
+﻿namespace GymBackend.UseCases.Common.Dtos.Workout;
+
+/// <summary>
+/// Set dto.
+/// </summary>
+public record SetsDto
+{
+    /// <summary>
+    /// Id.
+    /// </summary>
+    public Guid Id { get; init; }
+
+    /// <summary>
+    /// Number of repetitions.
+    /// </summary>
+    public int Reps { get; init; }
+
+    /// <summary>
+    /// The weight of the projectile.
+    /// </summary>
+    public double Weight { get; init; }
+
+    /// <summary>
+    /// User Id.
+    /// </summary>
+    public Guid CreatedById { get; init; }
+
+    /// <summary>
+    /// Created At.
+    /// </summary>
+    public DateTime CreatedAt { get; init; }
+}
