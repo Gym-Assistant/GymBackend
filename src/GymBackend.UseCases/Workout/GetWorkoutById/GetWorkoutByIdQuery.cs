3be9d3f18af15d2e8c2487f185f8c9201db97a18
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-08-28 01:42:59 +0700

    Add querys for get workout by id #9

---
+++ src/GymBackend.UseCases/Workout/GetWorkoutById/GetWorkoutByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using GymBackend.UseCases.Common.Dtos.Workout;
+using MediatR;
+
+namespace GymBackend.UseCases.Workout.GetWorkoutById;
+
+/// <summary>
+/// Get workout by id.
+/// </summary>
+public record GetWorkoutByIdQuery : IRequest<WorkoutDto>
+{
+    /// <summary>
+    /// Workout id.
+    /// </summary>
+    public Guid WorkoutId { get; init; }
+}
---
+++ src/GymBackend.UseCases/Workout/GetWorkoutById/GetWorkoutByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using GymBackend.Infrastructure.Abstractions.Interfaces;
+using GymBackend.UseCases.Common.BaseHandlers;
+using GymBackend.UseCases.Common.Dtos.Workout;
+using MediatR;
+using Saritasa.Tools.EFCore;
+
+namespace GymBackend.UseCases.Workout.GetWorkoutById;
+
+
+/// <summary>
+/// Handler for <see cref="GetWorkoutByIdQuery"/>.
+/// </summary>
+public class GetWorkoutByIdQueryHandler : BaseQueryHandler, IRequestHandler<GetWorkoutByIdQuery, WorkoutDto>
+{
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public GetWorkoutByIdQueryHandler(IMapper mapper, IAppDbContext dbContext) : base(mapper, dbContext)
+    {
+    }
+
+    /// <inheritdoc />
+    public async Task<WorkoutDto> Handle(GetWorkoutByIdQuery request, CancellationToken cancellationToken)
+    {
+        var workout = await DbContext.Workouts.ProjectTo<WorkoutDto>(Mapper.ConfigurationProvider)
+            .GetAsync(workout => workout.Id == request.WorkoutId, cancellationToken);
+
+        return workout;
+    }
+}
