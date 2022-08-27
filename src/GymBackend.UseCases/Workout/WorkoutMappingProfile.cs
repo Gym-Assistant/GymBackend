e9e4ac4f1d36182004a2a73df48fae5edef705b4
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-08-28 02:35:05 +0700

    Add profile for workout usecases #9

---
+++ src/GymBackend.UseCases/Workout/WorkoutMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using GymBackend.Domain.Workouts;
+using GymBackend.UseCases.Common.Dtos.Workout;
+
+namespace GymBackend.UseCases.Workout;
+
+/// <summary>
+/// Workout mapping profile.
+/// </summary>
+public class WorkoutMappingProfile : Profile
+{
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public WorkoutMappingProfile()
+    {
+        // Entity -> Dto.
+        CreateMap<Domain.Workouts.Workout, WorkoutDto>();
+        CreateMap<Domain.Workouts.Workout, LightWorkoutDto>();
+        CreateMap<Exercise, LightExerciseDto>();
+        CreateMap<Sets, SetsDto>();
+        CreateMap<TrainSession, TrainSessionDto>();
+    }
+}
