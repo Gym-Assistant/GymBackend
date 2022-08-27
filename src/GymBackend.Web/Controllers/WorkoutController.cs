176c1a8a4df36aeee10a916c0add9799784b30ce
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-08-28 02:41:26 +0700

    Add Api for works with workouts #9

---
+++ src/GymBackend.Web/Controllers/WorkoutController.cs
@@ -0,0 +1,83 @@
+﻿using GymBackend.Infrastructure.Abstractions.Interfaces;
+using GymBackend.UseCases.Common.Dtos.Workout;
+using GymBackend.UseCases.Workout.EndWorkout;
+using GymBackend.UseCases.Workout.GetAllWorkouts;
+using GymBackend.UseCases.Workout.GetWorkoutById;
+using GymBackend.UseCases.Workout.RemoveWorkout;
+using GymBackend.UseCases.Workout.StartWorkout;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Saritasa.Tools.Common.Pagination;
+
+namespace GymBackend.Web.Controllers;
+
+/// <summary>
+/// Workout controller.
+/// </summary>
+[ApiController]
+[Route("api/workout")]
+[ApiExplorerSettings(GroupName = "Workout")]
+[Authorize]
+public class WorkoutController : ControllerBase
+{
+    private readonly IMediator mediator;
+    private readonly ILoggedUserAccessor loggedUserAccessor;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public WorkoutController(IMediator mediator, ILoggedUserAccessor loggedUserAccessor)
+    {
+        this.mediator = mediator;
+        this.loggedUserAccessor = loggedUserAccessor;
+    }
+
+    /// <summary>
+    /// Get all workouts.
+    /// </summary>
+    /// <param name="query">Query.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>Paged list workout.</returns>
+    [HttpGet]
+    public async Task<PagedListMetadataDto<LightWorkoutDto>> GetWorkouts([FromQuery] GetAllWorkoutsQuery query, CancellationToken cancellationToken)
+    {
+        query = query with { UserId = loggedUserAccessor.GetCurrentUserId() };
+        return await mediator.Send(query, cancellationToken);
+    }
+
+    /// <summary>
+    /// Get workout by WorkoutId.
+    /// </summary>
+    /// <returns>Workout.</returns>
+    [HttpGet("{workoutId}")]
+    public async Task<WorkoutDto> GetWorkoutById([FromRoute] Guid workoutId, CancellationToken cancellationToken) =>
+        await mediator.Send(new GetWorkoutByIdQuery { WorkoutId = workoutId }, cancellationToken);
+
+    /// <summary>
+    /// Start workout.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>New workout id.</returns>
+    [HttpPost]
+    public async Task<Guid> StartNewWorkout(CancellationToken cancellationToken) =>
+        await mediator.Send(new StartWorkoutCommand(), cancellationToken);
+
+    /// <summary>
+    /// End workout.
+    /// </summary>
+    /// <param name="workoutId">WorkoutId.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    [HttpPost("{workoutId}")]
+    public async Task EndWorkout([FromRoute] Guid workoutId, CancellationToken cancellationToken) =>
+        await mediator.Send(new EndWorkoutCommand(workoutId), cancellationToken);
+
+    /// <summary>
+    /// Remove workout by Id.
+    /// </summary>
+    /// <param name="workoutId">WorkoutId.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    [HttpDelete("{workoutId}")]
+    public async Task RemoveWorkoutById([FromRoute] Guid workoutId, CancellationToken cancellationToken) =>
+        await mediator.Send(new RemoveWorkoutByIdCommand(workoutId), cancellationToken);
+}
