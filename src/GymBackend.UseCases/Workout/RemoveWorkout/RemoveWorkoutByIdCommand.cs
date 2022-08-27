af79da5d61c068ae3a9b2c2864447487b52916bf
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-08-28 02:36:04 +0700

    Add command for remove workout #9

---
+++ src/GymBackend.UseCases/Workout/RemoveWorkout/RemoveWorkoutByIdCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GymBackend.UseCases.Workout.RemoveWorkout;
+
+/// <summary>
+/// Remove workout by id.
+/// </summary>
+/// <param name="WorkoutId">Workout Id.</param>
+public record RemoveWorkoutByIdCommand(Guid WorkoutId) : IRequest;
