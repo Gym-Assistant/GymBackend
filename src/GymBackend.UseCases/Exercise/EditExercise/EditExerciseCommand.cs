d37b4b66e802e527e25b13e0743b8e731be0ac93
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-08-28 16:38:47 +0700

    Add commands for edit exersice by id #10

---
+++ src/GymBackend.UseCases/Exercise/EditExercise/EditExerciseCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+
+namespace GymBackend.UseCases.Exercise.EditExercise;
+
+/// <summary>
+/// Edit exercise command.
+/// </summary>
+public record EditExerciseCommand : IRequest
+{
+    /// <summary>
+    /// Id.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Name of exercise.
+    /// </summary>
+    [Required]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Description of exercise.
+    /// </summary>
+    public string Description { get; set; }
+}
