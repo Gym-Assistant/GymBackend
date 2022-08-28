411bee1050ad2390861e0895d96667f54e22482e
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-08-28 16:38:11 +0700

    Add commands for add exercise to workout #10

---
+++ src/GymBackend.UseCases/Exercise/AddExerciseToWorkout/AddExerciseToWorkoutCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+
+namespace GymBackend.UseCases.Exercise.AddExerciseToWorkout;
+
+/// <summary>
+/// Add exercise to workout.
+/// </summary>
+public record AddExerciseToWorkoutCommand : IRequest
+{
+    /// <summary>
+    /// Exercise id.
+    /// </summary>
+    [Required]
+    public Guid ExerciseId { get; init; }
+
+    /// <summary>
+    /// Workout id.
+    /// </summary>
+    [Required]
+    public Guid WorkoutId { get; init; }
+}
---
+++ src/GymBackend.UseCases/Exercise/AddExerciseToWorkout/AddExerciseToWorkoutCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using GymBackend.Infrastructure.Abstractions.Interfaces;
+using GymBackend.UseCases.Common.BaseHandlers;
+using MediatR;
+using Saritasa.Tools.Domain.Exceptions;
+using Saritasa.Tools.EFCore;
+
+namespace GymBackend.UseCases.Exercise.AddExerciseToWorkout;
+
+/// <summary>
+/// Handler for <see cref="AddExerciseToWorkoutCommand"/>.
+/// </summary>
+public class AddExerciseToWorkoutCommandHandler : BaseCommandHandler, IRequestHandler<AddExerciseToWorkoutCommand>
+{
+    private readonly ILoggedUserAccessor loggedUserAccessor;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public AddExerciseToWorkoutCommandHandler(IMapper mapper, IAppDbContext dbContext, ILoggedUserAccessor loggedUserAccessor)
+        : base(mapper, dbContext)
+    {
+        this.loggedUserAccessor = loggedUserAccessor;
+    }
+
+    /// <inheritdoc />
+    public async Task<Unit> Handle(AddExerciseToWorkoutCommand request, CancellationToken cancellationToken)
+    {
+        var workout = await DbContext.Workouts.GetAsync(workout => workout.Id == request.WorkoutId, cancellationToken);
+        var exercise = await DbContext.Exercises.GetAsync(exercise => exercise.Id == request.ExerciseId, cancellationToken);
+
+        // We cannot add exercise to workout when its not our workout.
+        // But we can add not out exercise to our workout.
+        if (workout.CreatedById != loggedUserAccessor.GetCurrentUserId())
+        {
+            throw new ForbiddenException("You are not allowed to add exercise to this workout");
+        }
+
+        workout.Exercises.Add(exercise);
+        await DbContext.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
