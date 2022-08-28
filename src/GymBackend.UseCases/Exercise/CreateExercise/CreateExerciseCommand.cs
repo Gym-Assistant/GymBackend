1ffdc869ee236dba0b1296f0269418eb9b5a46e3
Nikita Gorovenko <nikita.gorovenko@saritasa.com>
2022-08-28 16:38:30 +0700

    Add command for create new exersice #10

---
+++ src/GymBackend.UseCases/Exercise/CreateExercise/CreateExerciseCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+
+namespace GymBackend.UseCases.Exercise.CreateExercise;
+
+/// <summary>
+/// Create exercise command.
+/// </summary>
+/// <param name="Name">Name of exercise.</param>
+/// <param name="Description">Description.</param>
+public record CreateExerciseCommand() : IRequest<Guid>
+{
+    /// <summary>
+    /// Name of exercise.
+    /// </summary>
+    [Required]
+    public string Name { get; init; }
+
+    /// <summary>
+    /// Description.
+    /// </summary>
+    public string Description { get; init; }
+}
