3d369e4d698c76c691a069e2c7934127484e47c5
alexgoryach <alexandr.goryach@gmail.com>
2023-04-23 20:05:39 +0700

    Remove use-cases comments refactoring

--- src/FoodBackend.UseCases/CourseMeal/RemoveElementaryFromCourseMeal/RemoveElementaryFromCourseMealCommand.cs
+++ src/FoodBackend.UseCases/CourseMeal/RemoveElementaryFromCourseMeal/RemoveElementaryFromCourseMealCommand.cs
@@ -5,4 +5,6 @@ namespace FoodBackend.UseCases.CourseMeal.RemoveElementaryFromCourseMeal;
 /// <summary>
 /// Remove food elementary from course meal command.
 /// </summary>
+/// <param name="FoodElementaryId">Food elementary id.</param>
+/// <param name="CourseMealId">Course meal id.</param>
 public record RemoveElementaryFromCourseMealCommand(Guid FoodElementaryId, Guid CourseMealId) : IRequest;
--- src/FoodBackend.UseCases/FoodRecipe/RemoveIngredientFromRecipe/RemoveIngredientFromRecipeCommand.cs
+++ src/FoodBackend.UseCases/FoodRecipe/RemoveIngredientFromRecipe/RemoveIngredientFromRecipeCommand.cs
@@ -2,4 +2,9 @@
 
 namespace FoodBackend.UseCases.FoodRecipe.RemoveIngredientFromRecipe;
 
+/// <summary>
+/// Remove food elementary from recipe command.
+/// </summary>
+/// <param name="FoodRecipeId">Food recipe id.</param>
+/// <param name="FoodElementaryId">Food elementary id.</param>
 public record RemoveIngredientFromRecipeCommand(Guid FoodRecipeId, Guid FoodElementaryId) : IRequest;
