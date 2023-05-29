4cc50f55bcbac838a01f691df08f884277957db7
alexgoryach <alexandr.goryach@gmail.com>
2023-05-29 15:28:11 +0700

    Added changing characteristic while changing weight food 22

--- src/FoodBackend.UseCases/FoodRecipe/ChangeElementaryWeightInFoodRecipe/ChangeElementaryWeightInRecipeCommandHandler.cs
+++ src/FoodBackend.UseCases/FoodRecipe/ChangeElementaryWeightInFoodRecipe/ChangeElementaryWeightInRecipeCommandHandler.cs
@@ -14,21 +14,27 @@ namespace FoodBackend.UseCases.FoodRecipe.ChangeElementaryWeightInFoodRecipe;
 internal class ChangeElementaryWeightInRecipeCommandHandler : BaseCommandHandler, IRequestHandler<ChangeElementaryWeightInRecipeCommand>
 {
     private readonly ILoggedUserAccessor loggedUserAccessor;
+    private readonly IChangeRecipeCharacteristicSum changeRecipeCharacteristicSum;
 
     /// <summary>
     /// Constructor.
     /// </summary>
     public ChangeElementaryWeightInRecipeCommandHandler(IMapper mapper, IAppDbContext dbContext,
-        ILoggedUserAccessor loggedUserAccessor) : base(mapper, dbContext)
+        ILoggedUserAccessor loggedUserAccessor, IChangeRecipeCharacteristicSum changeRecipeCharacteristicSum) : base(mapper, dbContext)
     {
         this.loggedUserAccessor = loggedUserAccessor;
+        this.changeRecipeCharacteristicSum = changeRecipeCharacteristicSum;
     }
 
     /// <inheritdoc/>
     public async Task Handle(ChangeElementaryWeightInRecipeCommand request, CancellationToken cancellationToken)
     {
+        const bool increaseCharacteristicSumStatement = true;
+        const bool decreaseCharacteristicSumStatement = false;
         var foodRecipe = await DbContext.FoodRecipes
             .Include(recipe =>  recipe.Ingredients)
+            .Include(recipe => recipe.IngredientWeights)
+            .Include(recipe => recipe.CharacteristicValuesSum)
             .GetAsync(recipe => recipe.Id == request.FoodRecipeId, cancellationToken);
         if (foodRecipe.UserId != loggedUserAccessor.GetCurrentUserId())
         {
@@ -36,6 +42,7 @@ internal class ChangeElementaryWeightInRecipeCommandHandler : BaseCommandHandler
         }
         
         var foodElementary = await DbContext.FoodElementaries
+            .Include(elementary => elementary.Characteristics)
             .GetAsync(foodElementary => foodElementary.Id == request.FoodElementaryId, cancellationToken);
         if (!foodRecipe.Ingredients.Contains(foodElementary))
         {
@@ -44,7 +51,11 @@ internal class ChangeElementaryWeightInRecipeCommandHandler : BaseCommandHandler
         var elementaryWeight = await DbContext.FoodElementaryWeights
             .GetAsync(foodElementaryWeight => foodElementaryWeight.FoodElementaryId == request.FoodElementaryId &&
                                               foodElementaryWeight.FoodRecipeId == request.FoodRecipeId, cancellationToken);
+        await changeRecipeCharacteristicSum.ChangeRecipeCharacteristic(foodRecipe, foodElementary, elementaryWeight.Weight,
+            decreaseCharacteristicSumStatement, cancellationToken);
         elementaryWeight.Weight = request.Weight;
+        await changeRecipeCharacteristicSum.ChangeRecipeCharacteristic(foodRecipe, foodElementary, request.Weight,
+            increaseCharacteristicSumStatement, cancellationToken);
         await DbContext.SaveChangesAsync(cancellationToken);
     }
 }
