7f6d73b874454f7b7e966beb8e0c1658777f2b42
alexgoryach <alexandr.goryach@gmail.com>
2023-05-30 13:02:24 +0700

    Changed recipe DTO with detail information

---
+++ src/FoodBackend.UseCases.Common/Dtos/FoodElementaryInRecipeDto.cs
@@ -0,0 +1,17 @@
+﻿namespace FoodBackend.UseCases.Common.Dtos;
+
+/// <summary>
+/// Food elementary weight in recipe DTO.
+/// </summary>
+public record FoodElementaryInRecipeDto
+{
+    /// <summary>
+    /// Food elementary with characteristics.
+    /// </summary>
+    public DetailFoodElementaryDto FoodElementary { get; init; }
+    
+    /// <summary>
+    /// Food elementary weight value.
+    /// </summary>
+    public double ElementaryWeight { get; init; }
+}
