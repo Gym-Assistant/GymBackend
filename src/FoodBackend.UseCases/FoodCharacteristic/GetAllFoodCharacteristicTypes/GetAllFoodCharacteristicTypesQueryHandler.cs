f0e16d8ef5a3bee0613cfcff91fca1dd2cc47a20
alexgoryach <alexandr.goryach@gmail.com>
2023-06-12 13:56:35 +0700

    Added characteristic types visibility and search by food-40

--- src/FoodBackend.UseCases/FoodCharacteristic/GetAllFoodCharacteristicTypes/GetAllFoodCharacteristicTypesQueryHandler.cs
+++ src/FoodBackend.UseCases/FoodCharacteristic/GetAllFoodCharacteristicTypes/GetAllFoodCharacteristicTypesQueryHandler.cs
@@ -15,22 +15,35 @@ namespace FoodBackend.UseCases.FoodCharacteristic.GetAllFoodCharacteristicTypes;
 internal class GetAllFoodCharacteristicTypesQueryHandler : BaseQueryHandler,
     IRequestHandler<GetAllFoodCharacteristicTypesQuery, PagedListMetadataDto<FoodCharacteristicTypeDto>>
 {
+    private readonly ILoggedUserAccessor loggedUserAccessor;
+
     /// <summary>
     /// Constructor.
     /// </summary>
-    public GetAllFoodCharacteristicTypesQueryHandler(IMapper mapper, IAppDbContext dbContext) : base(mapper, dbContext)
+    public GetAllFoodCharacteristicTypesQueryHandler(IMapper mapper, IAppDbContext dbContext,
+        ILoggedUserAccessor loggedUserAccessor) : base(mapper, dbContext)
     {
+        this.loggedUserAccessor = loggedUserAccessor;
     }
 
     /// <inheritdoc />
     public async Task<PagedListMetadataDto<FoodCharacteristicTypeDto>> Handle(GetAllFoodCharacteristicTypesQuery request,
         CancellationToken cancellationToken)
     {
+        const bool isDefaultCharacteristicStatement = true;
         var foodCharacteristicTypesQuery = DbContext.FoodCharacteristicTypes
-            .ProjectTo<FoodCharacteristicTypeDto>(Mapper.ConfigurationProvider);
-        if (request.UserId != null)
+            .ProjectTo<FoodCharacteristicTypeDto>(Mapper.ConfigurationProvider)
+            .Where(foodCharacteristic => foodCharacteristic.IsDefault == isDefaultCharacteristicStatement);
+        if (loggedUserAccessor.IsAuthenticated())
         {
-            foodCharacteristicTypesQuery = foodCharacteristicTypesQuery.Where(foodCharacteristic => foodCharacteristic.UserId == request.UserId);
+            foodCharacteristicTypesQuery = foodCharacteristicTypesQuery.Union(DbContext.FoodCharacteristicTypes
+                .ProjectTo<FoodCharacteristicTypeDto>(Mapper.ConfigurationProvider)
+                .Where(foodCharacteristic => foodCharacteristic.UserId == loggedUserAccessor.GetCurrentUserId()));
+        }
+        if (request.SearchBy != null)
+        {
+            foodCharacteristicTypesQuery = foodCharacteristicTypesQuery
+                .Where(dto => dto.Name.ToLower().Contains(request.SearchBy.ToLower()));
         }
         var pagedFoodCharacteristicTypesQuery = await
             EFPagedListFactory.FromSourceAsync(foodCharacteristicTypesQuery, request.Page, request.PageSize, 
